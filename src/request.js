d58caccdbacfeb113fb761f95730d96b72d43b7d
MoltenPL85 <bugaev.maxim.85@gmail.com>
2018-06-20 13:02:17 +0500

    Hangman on github

---
+++ src/request.js
@@ -0,0 +1,40 @@
+const getPuzzle = async wordCount => {
+  const response = await fetch(
+    `//puzzle.mead.io/puzzle?wordCount=${wordCount}`,
+  );
+
+  if (response.status === 200) {
+    const data = await response.json();
+    return data.puzzle;
+  } else {
+    throw new Error('Unable to get puzzle');
+  }
+};
+
+const getCurrentCountry = async () => {
+  const location = await getLocation();
+  return getCountry(location.country);
+};
+
+const getCountry = async code => {
+  const response = await fetch('//restcountries.eu/rest/v2/all');
+
+  if (response.status === 200) {
+    const data = await response.json();
+    return data.find(country => country.alpha2Code === code);
+  } else {
+    throw new Error('Somethind went wrong');
+  }
+};
+
+// https - fetch not work
+const getLocation = async () => {
+  const response = await fetch('//ipinfo.io/json?dcf535425ef764');
+  if (response.status === 200) {
+    return response.json();
+  } else {
+    throw new Error('Some error');
+  }
+};
+
+export { getPuzzle as default };
